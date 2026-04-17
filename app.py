ed74e4913cd43bec310a9392732cce35fa11d466
Kishore <stonebench04@gmail.com>
2026-04-17 19:02:49 +0530

    fixed structure

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Book AI Project Running!"
-    if __name__ == "__main__":
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
